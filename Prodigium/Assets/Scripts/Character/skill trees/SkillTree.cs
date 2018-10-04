0ebd15c19122a266cb5fe41ebd8416b96668fcc9
haley knights <lt.knights@live.com.au>
2018-10-04 12:38:12 +1000

    skill tree set up

---
+++ Prodigium/Assets/Scripts/Character/skill trees/SkillTree.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTree : MonoBehaviour {
+
+    public int SkillPoints;
+
+    [SerializeField]
+    private GameObject m_baseSkills;
+
+    private bool Skill = false;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        SkillPoints = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+   void SKillClcikedOn()
+    {
+        //game object light up and turn on code goes here
+        Skill = true;
+        
+
+
+    }
+
+    //in sepreate skript
+    void lightup()
+    {
+        if(Skill)
+        {
+           //set what skill does
+        }
+    }
+
+}
