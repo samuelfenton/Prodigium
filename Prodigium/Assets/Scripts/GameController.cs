edc1118e7dc149ee1bde7a151b70f49b9dbd818c
haley knights <lt.knights@live.com.au>
2018-10-04 11:02:59 +1000

    player controller start

---
+++ Prodigium/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+
+    GameObject p_pController;
+    GameObject p_aiController;
+    GameObject p_questController;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
