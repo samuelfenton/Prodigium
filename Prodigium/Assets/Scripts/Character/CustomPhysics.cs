0673ed2a4a10c5830c8e329e9bef608d2da072f5
Samuel Fenton <>
2018-10-04 12:31:31 +1000

    Basic player setup Created Layer manager Unity layer of "Enviroment" added

--- Prodigium/Assets/Scripts/Character/CustomPhysics.cs
+++ Prodigium/Assets/Scripts/Character/CustomPhysics.cs
@@ -2,12 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CapsuleCollider))]
 public class CustomPhysics : MonoBehaviour 
 {
-	public void UpdatePhysics()
-	{
+    public static float GRAVITY = -9.8f;
 
+    public Vector3 m_velocity = Vector3.zero;
+    private CapsuleCollider m_capsuleCollider = null;
 
-	}
+    private Vector3 m_colliderExtents = Vector3.zero;   
+
+    private void Start()
+    {
+        m_capsuleCollider = GetComponent<CapsuleCollider>();
+        m_colliderExtents.x = m_colliderExtents.z = m_capsuleCollider.radius;
+        m_colliderExtents.y = m_capsuleCollider.height / 2.0f;
+    }
+
+    public void UpdatePhysics()
+    {
+        // Add gravity
+        m_velocity.y += GRAVITY * Time.fixedDeltaTime;
+
+        if (IsGrounded() && m_velocity.y < 0)
+        {
+            m_velocity.y = 0;
+            SnapToGround();
+        }
+
+        //Local translat, so moving forwards is simply vector3.forward, not translate.forward
+        transform.Translate(m_velocity * Time.fixedDeltaTime);
+    }
+
+    public bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, m_colliderExtents.y, LayerController.m_enviroment);
+    }
+
+    public void SnapToGround()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, -Vector3.up, out hit, m_colliderExtents.y, LayerController.m_enviroment))
+        {
+            Vector3 position = transform.position;
+            position.y = hit.point.y + m_colliderExtents.y;
+            transform.position = position;
+        }
+    }
 
 }
--- Prodigium/Assets/Scripts/Character/Player/Player.cs
+++ Prodigium/Assets/Scripts/Character/Player/Player.cs
@@ -6,7 +6,19 @@ public class Player : Character
 {
     protected override void FixedUpdate()
     {
-		base.FixedUpdate();
+        //Movement
+        Vector3 velocity = m_customPhysics.m_velocity;
+
+        velocity = m_customPhysics.m_velocity;
+        velocity.z = m_forwardSpeed * Input.GetAxisRaw("Vertical");
+        velocity.x = m_strafingSpeed * Input.GetAxisRaw("Horizontal");
+
+        m_customPhysics.m_velocity = velocity;
+
+        //Rotation
+        transform.RotateAround(transform.position, Vector3.up, Input.GetAxisRaw("MouseX") * m_rotationSpeed * Time.fixedDeltaTime);
+
+        base.FixedUpdate();
     }
 
 }
