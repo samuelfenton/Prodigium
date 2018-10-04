1fe585da3259abf3d8bd454d7a11f1e243f2e9ee
Samuel Fenton <>
2018-10-04 11:50:38 +1000

    Setup Base character controller

---
+++ Prodigium/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour 
+{
+	public CustomPhysics m_customPhysics = null;
+
+	public float m_maxHealth = 1.0f;
+    
+    private float m_currentHealth = 1.0f;
+    
+	protected virtual void Start()
+	{
+		//m_customPhysics = GetComponent(
+	}
+
+	protected virtual void FixedUpdate()
+	{
+
+	}
+
+	public bool IsAlive()
+	{
+		return m_currentHealth <= 0;
+	}
+}
