9057820c8de4ef9a8441e63e05e720877b947d19
Samuel Fenton <>
2018-10-19 13:44:18 +1100

    asdasd

--- Prodigium/Assets/Scripts/Character/Character.cs
+++ Prodigium/Assets/Scripts/Character/Character.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CustomPhysics))]
-public class Character : MonoBehaviour 
+public class Character : MonoBehaviour
 {
-	public CustomPhysics m_customPhysics = null;
+    public CustomPhysics m_customPhysics = null;
 
     [Header("Health and Shizz")]
-	public float m_maxHealth = 1.0f;
+    public float m_maxHealth = 1.0f;
     [SerializeField]
     protected float m_currentHealth = 1.0f;
 
@@ -19,16 +19,6 @@ public class Character : MonoBehaviour
     [SerializeField]
     public float m_strafingSpeed = 1.0f;
     [SerializeField]
-<<<<<<< HEAD
-    protected float m_rotationSpeed = 1.0f;
-
-    [Header("Attacking and Shizz")]
-    public float m_attackRange = 1.0f;
-
-    [Header("Stats modifiers")]
-    public float m_attackMod = 0.0f;
-    public float m_speedMod = 0.0f;
-=======
     public float m_rotationSpeed = 1.0f;
 
     [Header("Attacking/Armour and Shizz")]
@@ -40,43 +30,37 @@ public class Character : MonoBehaviour
     public float m_attackMod = 0.0f;
     public float m_speedMod = 0.0f;
     public float m_armourMod = 0.0f;
->>>>>>> 48ca6a461aab5a941e8c455c4d908117892230c9
     public float m_healthMod = 0.0f;
 
     public float m_naturalResistance = 0;
 
     protected virtual void Start()
-	{
+    {
         m_customPhysics = GetComponent<CustomPhysics>();
-	}
+    }
 
-	protected virtual void FixedUpdate()
-	{
+    protected virtual void FixedUpdate()
+    {
         m_customPhysics.UpdatePhysics();
     }
 
-	public bool IsAlive()
-	{
-		return m_currentHealth <= 0;
-	}
+    public bool IsAlive()
+    {
+        return m_currentHealth <= 0;
+    }
 
     public void ModifyHealth(float p_value)
     {
         m_currentHealth += p_value;
     }
-<<<<<<< HEAD
-
-    public void DealDamage(Character p_character, Weapon p_weapon)
-=======
 
     public void DealDamage(Character p_attackCharacter)
->>>>>>> 48ca6a461aab5a941e8c455c4d908117892230c9
     {
         float damage = 0.0f;
         damage = p_attackCharacter.m_currentWeapon != null ? p_attackCharacter.m_currentWeapon.m_weaponDamage : 1; //Flat damage
         damage *= 1 + p_attackCharacter.m_attackMod; //Attack modified
 
-
+        damage *= ((m_naturalResistance + m_armourMod) * m_armourMod)
         //m_currentHealth -= (1 + p_attackCharacter.m_attackMod) * p_weaponDamage;
     }
 }
